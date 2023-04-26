0d113a795472cad9b24d9e17d849ea7a84d973d2
saad ablat <saad.ablat@gmail.com>
2023-04-26 13:45:39 +0100

    Configure signalR

--- WorkoutTrackerWebsite/Program.cs
+++ WorkoutTrackerWebsite/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddSignalR().AddAzureSignalR();
 
 
 
