db4d254ea6818725d3f0e9e7df52dadf0f601feb
saad ablat <saad.ablat@gmail.com>
2023-04-26 13:59:34 +0100

    the app didn't work so I added .addhubOptions on program.cs

--- WorkoutTrackerWebsite/Program.cs
+++ WorkoutTrackerWebsite/Program.cs
@@ -9,8 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddServerSideBlazor()
+    .AddHubOptions(options =>
+    {
+        options.ClientTimeoutInterval = TimeSpan.FromSeconds(60);
+        options.HandshakeTimeout = TimeSpan.FromSeconds(30);
+    });
 builder.Services.AddSignalR().AddAzureSignalR();
+
 
 
 
